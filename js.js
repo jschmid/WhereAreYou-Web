887cee2c875f828b4546e110ec5017967ceaee33
Jonas Schmid <jonas.schmid@gmail.com>
2013-01-12 18:30:13 +0100

    New firebase structure

--- js.js
+++ js.js
@@ -1,6 +1,9 @@
+
 var map;
 var myHandle;
-var browserSupportFlag =  new Boolean();
+var myPosition;
+var browserSupportFlag = new Boolean();
+var markers = {};
 
 var siberia = new google.maps.LatLng(60, 105);
 var newyork = new google.maps.LatLng(40.69847032728747, -73.9514422416687);
@@ -19,49 +22,77 @@ function initialize() {
   
   var f = new Firebase("https://whereareyou.firebaseio.com");
   
-  if(childname == "") {
+  if (childname == "") {
     var child = f.push();
     document.location = document.location + child.name();
+    return;
   }
   
   var child = f.child(childname);
-  myHandle = child.push();
-  myHandle.removeOnDisconnect();
   
   child.on("child_added", function (snapshot) {
-    var val = snapshot.val();
-    var pri = snapshot.getPriority();
-    var d = new Date(pri * 1000);
+    var person = snapshot.val();
+    var name = person.name;
     
-    console.log(val);
-    
-    var myLatlng = new google.maps.LatLng(val.lat, val.long);
-    var marker = new google.maps.Marker({
-        position : myLatlng,
-        map : map,
-        title : val.name
-      });
-    
-    var contentString = '<div id="content">' +
-      '<h1 id="firstHeading" class="firstHeading">' + val.name + '</h1>' +
-      '<div id="bodyContent">' +
-      d +
-      '</div>' +
-      '</div>';
-    
-    var infowindow = new google.maps.InfoWindow({
-        content : contentString
-      });
-    
-    google.maps.event.addListener(marker, 'click', function () {
-      infowindow.open(map, marker);
+    var positionRef = snapshot.ref().child("position");
+    positionRef.on("value", function (sPosition) {
+      var position = sPosition.val();
+      
+      console.log(position);
+      
+      if (position == null) {
+        return;
+      }
+      
+      var lat = position.lat;
+      var longitude = position.long;
+      var datetime = position.datetime;
+      var date = new Date(datetime * 1000);
+      var parentName = sPosition.ref().parent().name();
+      
+      var myLatlng = new google.maps.LatLng(lat, longitude);
+      
+      var marker = markers[parentName];
+      
+      if (marker == undefined) {
+        marker = new google.maps.Marker({
+            position : myLatlng,
+            map : map,
+            title : name
+          });
+        
+        var contentString = '<div id="content">' +
+          '<h1 id="firstHeading" class="firstHeading">' + name + '</h1>' +
+          '<div id="bodyContent">' +
+          date +
+          '</div>' +
+          '</div>';
+        
+        var infowindow = new google.maps.InfoWindow({
+            content : contentString
+          });
+        
+        google.maps.event.addListener(marker, 'click', function () {
+          infowindow.open(map, marker);
+        });
+        markers[parentName] = marker;
+        
+      } else {
+        marker.setPosition(myLatlng);
+      }
+      
     });
+    
   });
   
-// Try W3C Geolocation (Preferred)
-  if(navigator.geolocation) {
+  myHandle = child.push();
+  myHandle.child("name").set("Web");
+  myPosition = myHandle.child("position");
+  
+  // Try W3C Geolocation (Preferred)
+  if (navigator.geolocation) {
     browserSupportFlag = true;
-    navigator.geolocation.getCurrentPosition(handleGeolocation, function() {
+    navigator.geolocation.getCurrentPosition(handleGeolocation, function () {
       handleNoGeolocation(browserSupportFlag);
     });
   }
@@ -73,28 +104,25 @@ function initialize() {
 }
 
 function handleGeolocation(position) {
-  console.log(position);
-  alert("got position");
-  
   var ts = Math.round((new Date()).getTime() / 1000);
-
-  myHandle.setWithPriority({
+  
+  myPosition.set({
     "lat" : position.coords.latitude,
     "long" : position.coords.longitude,
-    "name" : "Web"
-  }, ts);
+    "datetime" : ts
+  });
   
   var myLocation = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
   map.setCenter(myLocation);
 }
 
 function handleNoGeolocation(errorFlag) {
-    if (errorFlag) {
-      alert("Geolocation service failed.");
-      initialLocation = newyork;
-    } else {
-      alert("Your browser doesn't support geolocation. We've placed you in Siberia.");
-      initialLocation = siberia;
-    }
-    map.setCenter(initialLocation);
+  if (errorFlag) {
+    alert("Geolocation service failed.");
+    initialLocation = newyork;
+  } else {
+    alert("Your browser doesn't support geolocation. We've placed you in Siberia.");
+    initialLocation = siberia;
   }
+  map.setCenter(initialLocation);
+}
