e29986453590f847ac76a54b7cd56f122a1ebb68
Jonas Schmid <jonas.schmid@gmail.com>
2013-03-12 21:03:14 +0100

    Updated publishing script

--- publish.sh
+++ publish.sh
@@ -1,14 +1,27 @@
+#!/bin/bash
+
+set -e
+
 git checkout master
-git merge dev
-git push origin master
 
-git checkout gh-pages
-git merge master
-cp index.html 404.html
-git add 404.html
-git rm publish.sh
-git commit -m "Publishing web"
-
-git push origin gh-pages
-
-git checkout dev
+rm -rf build
+mkdir build
+cd build
+mkdir files
+git clone git@github.com:jschmid/WhereAreYou-Web.git -b gh-pages gh-pages
+cd ..
+cp 404.css build/files/
+cp 404.html build/files/
+cp icon.png build/files/
+cp index.html build/files/
+coffee -o build/files -c 404.coffee index.coffee
+cd build
+cd gh-pages
+git rm *
+cd ..
+cp files/* gh-pages/
+cd gh-pages
+echo "w.schmid.pro" > CNAME
+git add *
+git commit -m "Publishing"
+git push
